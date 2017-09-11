be8622b6dbcb7ecba4549cc04a10b06c93604cc2
jasperbuss <jasper_buss@web.de>
2017-09-11 16:08:08 +0200

    Etappenrouter

--- routes/api.js
+++ routes/api.js
@@ -91,4 +91,55 @@ router.get('/load/route/:name/', function(req, res, next) {
     };
   });
 });
+router.post('/save/etappe/', function(req, res, next) {
+
+    // Set collection
+    var jsoncollection = db.get('jsoncollection');
+
+    // Submit to the DB
+    jsoncollection.insert({
+        "Etappenname" : req.body.nameroute,
+        "start" : req.body.start,
+        "ziel" : req.body.ziel
+        "Termine": {
+          "Anfangsdatum" : req.body.Anfangsdatum,
+          "Enddatum" : req.body.Enddatum
+        }
+    }, function (err, doc) {
+        if (err) {
+          res.status(500).end("Failed to write Etappa to Database");
+        }
+        else {
+          res.status(200).end("Successfully written Etappe to Database.");
+        }
+    });
+});
+
+/* GET stored Route */
+router.get('/load/etappe/:name/', function(req, res, next) {
+
+  // Set Collection
+  var collection = db.get('jsoncollection');
+
+    // Retrieve Entries with matching Name from Database. Compelete Entry is returned on match.
+    collection.find({routename: { $eq: req.params.name }},{},function(e,route){
+    // Check for connection/syntax errors
+    if(e){
+      res.status(500).end("Failed to retrieve results from Database.");
+    }else{
+      // Check if there is an Entry - else fail
+      if(etappe.length != 0){
+        // Entry is returned
+        res.send(route);
+      } else {
+        // No entry was found
+        res.status(404).end("No such Object in the Database.");
+      }
+    };
+  });
+});
+
+
+
+
 module.exports = router;
